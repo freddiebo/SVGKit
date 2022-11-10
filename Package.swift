09cc83b6367451f1b185caa5fe8613d75eb6a679
freddiebo <52605186+freddiebo@users.noreply.github.com>
2022-11-10 20:13:56 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "SVGKit",
     platforms: [
         .macOS(.v10_10),
-        .iOS(.v13),
-        .tvOS(.v13)
+        .iOS(.v11),
+        .tvOS(.v11)
     ],
     products: [
         .library(
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", .upToNextMinor(from: "3.7.0"))
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", .upToNextMajor(from: "3.7.0"))
     ],
     targets: [
         .target(
